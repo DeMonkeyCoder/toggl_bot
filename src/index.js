0c0daea83b9005a61d708b4d35ae36bb152a0897
Ali Mahdiyar <alimahdiyar77@gmail.com>
2024-01-29 01:57:29 +0330

    feat: time tracker features

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ require("log-timestamp");
 const dotenv = require("dotenv");
 
 const {Telegraf, Markup} = require("telegraf");
-const {message, editedMessage} = require("telegraf/filters");
+const {message} = require("telegraf/filters");
 
 const LocalSession = require("telegraf-session-local");
 
@@ -14,388 +14,109 @@ const TogglTrackAPI = require("./toggl_track_api");
 dotenv.config();
 
 const settings = {
-    debug: process.env.DEBUG === "true" || process.env.DEBUG === "1",
     botToken: process.env.BOT_TOKEN,
-    checkInterval: parseInt(process.env.CHECK_INTERVAL) || 0,
-    adminUsername: process.env.ADMIN_USERNAME, // for reporting problems
     databasePath: "data/sessions.json",
 };
 if (!settings.botToken) {
     throw new Error("env.BOT_TOKEN is not set");
 }
-if (settings.checkInterval < 10) {
-    throw new Error("env.CHECK_INTERVAL should be greater than 10");
-}
 
-if (settings.debug) {
-    console.debug("Starting with settings:", settings);
-}
-
-const timeouts = [];
 const localSession = new LocalSession({
     storage: LocalSession.storageFileAsync,
     database: settings.databasePath,
 });
 
-function dbGetTokenObj(db, sessionId, token) {
-    const tokens = db.chain().get("sessions").find({id: sessionId}).get("data").get("tokens").value() || [];
-    for (let i = 0; i < tokens.length; ++i) {
-        const tokenObj = tokens[i];
-        if (tokenObj.token === token) {
-            return tokenObj;
-        }
-    }
-}
-
-async function restoreTimerChecks(db) {
-    const data = db.get("sessions").value();
-    for (const session of data) {
-        for (const tokenObj of session.data.tokens) {
-            if (tokenObj.checking) {
-                await checkTimerStatus(session.id, tokenObj.token);
-            }
-        }
-    }
-}
-
-function initSession(session) {
-    if (!("tokens" in session)) {
-        session.tokens = [];
-    }
-}
-
-localSession.DB.then(restoreTimerChecks);
-
-function humanDuration(seconds) {
-    let hours = Math.floor(seconds / 3600);
-    seconds -= hours * 3600;
-    hours = hours ? `${hours}h ` : "";
-    let minutes = Math.floor(seconds / 60);
-    seconds -= minutes * 60;
-    minutes = minutes ? `${minutes}m ` : "";
-    seconds = seconds ? `${seconds}s ` : "";
-    return `${hours}${minutes}${seconds}`.trim();
-}
-
-
-const checkAgainReplyMarkup = Markup.inlineKeyboard([
-    Markup.button.callback("Check again", "check"),
-]);
-const startCheckingReplyMarkup = Markup.inlineKeyboard([
-    Markup.button.callback("Start checking continuously", "start_checking"),
-]);
-const stopCheckingReplyMarkup = Markup.inlineKeyboard([
-    Markup.button.callback("Stop", "stop_checking"),
-]);
-
-function hasToken(ctx) {
-    return ctx.session.tokens.length;
-}
-
-async function checkTimerStatus(sessionId, token) {
-    const db = localSession.DB;
-    const tokenObj = dbGetTokenObj(db, sessionId, token);
-    if (!tokenObj?.checking) {
-        return;
-    }
-    const currentTimer = await TogglTrackAPI.currentTimer(token);
-    if (currentTimer) {
-        checkTimerStatusSetTimeout(sessionId, token);
-        return;
-    }
-    delete tokenObj.checking;
-    await db.write();
-    const chatId = sessionId.split(":")[0];
-    await bot.telegram.sendMessage(chatId, "Timer was stopped!", checkAgainReplyMarkup);
-}
-
-function checkTimerStatusSetTimeout(sessionId, token) {
-    const timeout = setTimeout(async () => {
-        const index = timeouts.indexOf(timeout);
-        if (index !== -1) {
-            timeouts.splice(index, 1);
-        }
-        try {
-            await checkTimerStatus(sessionId, token);
-        } catch (e) {
-            console.error("checkTimerStatus", e);
-        }
-    }, settings.checkInterval * 1000);
-    timeouts.push(timeout);
-}
-
-async function checkHasToken(ctx, next) {
-    if (hasToken(ctx)) {
-        return next();
-    }
-    const msg = "There's no stored token. Send it to the chat first";
-    if (ctx.update.callback_query) {
-        await ctx.answerCbQuery(msg, {show_alert: true});
-    } else {
-        await ctx.sendMessage(msg);
-    }
-}
 
 async function setup(ctx, next) {
     const update = ctx.update;
     const message = update.message;
-    const callbackQuery = update.callback_query;
     let action = message ? `text:${message.text}` : null;
-    action ??= callbackQuery ? `action:${callbackQuery.data}` : null;
     action ??= "<empty>";
     action = action.slice(0, 30);
-    const fromId = (message || callbackQuery)?.from.id || "-";
-    const fromUsername = (message || callbackQuery)?.from.username || "-";
+    const fromId = message?.from.id || "-";
+    const fromUsername = message?.from.username || "-";
     const title = `Processing update [${ctx.update.update_id}] from [${fromId} @${fromUsername}] with text "${action}"`;
     console.time(title);
-    if (ctx.chat.type === "private") {
-        initSession(ctx.session);
-        ctx.sessionId = localSession.getSessionKey(ctx);
-        await next();
-    } else {
-        await ctx.sendMessage("Incorrect chat type");
-    }
+    ctx.session = ctx.session ?? {}
+    ctx.sessionId = localSession.getSessionKey(ctx);
+    await next();
     console.timeEnd(title);
 }
 
-function buildTimerMessage(tokenObj) {
-    const now = Math.floor(Date.now() / 1000);
-    const duration = humanDuration(now + tokenObj.lastDuration); // duration - negative unix timestamp
-    let msg = `Timer was started ${duration} ago\n`;
-    if (tokenObj.checking) {
-        msg += "Continuous checking is active";
+async function onMessageHandler(ctx) {
+    const command = ctx?.message?.text
+    if(!command) {
+        return
     }
-    return msg.trim();
-}
-
-async function commandCheckHandler(ctx) {
-    const tokenObj = ctx.session.tokens[0];
-    const currentTimer = await TogglTrackAPI.currentTimer(tokenObj.token);
-    if (!currentTimer) {
-        delete tokenObj.checking;
-        await ctx.sendMessage("Timer is not started", checkAgainReplyMarkup);
-        return;
+    if (command === 'e') {
+        const currentTimer = await TogglTrackAPI.currentTimer();
+        if (!currentTimer) {
+            return await ctx.sendMessage(`No active timer`);
+        }
+        const stoppedTimeEntry = await TogglTrackAPI.stopTracking(currentTimer, new Date(ctx.message.date * 1000).toISOString());
+        return await ctx.sendMessage(`Stopped time entry ${JSON.stringify(stoppedTimeEntry)}`);
     }
-    tokenObj.lastDuration = currentTimer.duration;
-    const msg = buildTimerMessage(tokenObj);
-    await ctx.sendMessage(msg, tokenObj.checking ? stopCheckingReplyMarkup : startCheckingReplyMarkup);
-}
-
-async function commandStartHandler(ctx) {
-    if (hasToken(ctx)) {
-        const tokenObj = ctx.session.tokens[0];
-        await ctx.sendMessage(
-            `I have a token stored for "${tokenObj.username}".\n` +
-            `But you can send me another one to replace it.\n` +
-            `/check — to perform a timer check`,
-        );
-        return;
+    if (command === 'd') {
+        const currentTimer = await TogglTrackAPI.currentTimer();
+        if (!currentTimer) {
+            return await ctx.sendMessage(`No active timer`);
+        }
+        await TogglTrackAPI.discardTracking(currentTimer);
+        return await ctx.sendMessage(`Discarded time entry`);
     }
-    await ctx.sendMessage(
-        "Send me a token.\n" +
-        "You can find it here: https://track.toggl.com/profile#api-token",
-    );
-}
-
-async function commandStopHandler(ctx) {
-    const tokenObj = ctx.session.tokens[0];
-    if (!tokenObj.checking) {
-        await ctx.sendMessage("Continuous checking is not active!");
-        return;
+    if (command === 's') {
+        if(!ctx.session.pid) {
+            return await ctx.sendMessage('pid not set');
+        }
+        const currentTimer = await TogglTrackAPI.currentTimer();
+        if (currentTimer) {
+            return await ctx.sendMessage(`Timer already running: ${JSON.stringify(currentTimer)}`);
+        }
+        const timeEntry = await TogglTrackAPI.startTracking({
+            pid: Number(ctx.session.pid),
+            start: new Date(ctx.message.date * 1000).toISOString()
+        });
+        return await ctx.sendMessage(`Started time entry ${JSON.stringify(timeEntry)}`);
     }
-    delete tokenObj.checking;
-    await ctx.sendMessage("Continuous checking was stopped");
-}
-
-async function commandEditHandler(ctx) {
-    await ctx.sendMessage("Simply send me a new token, I will replace the old one with it");
-}
-
-async function commandDeleteHandler(ctx) {
-    ctx.session.tokens = [];
-    await ctx.sendMessage("Token was deleted");
-}
-
-async function commandSettingsHandler(ctx) {
-    if (ctx.update.message.from.username !== settings.adminUsername) {
-        await ctx.sendMessage("You are not admin!");
-        return;
+    if (command === 'p') {
+        const projects = await TogglTrackAPI.projectsString()
+        return await ctx.sendMessage(projects, { parse_mode: 'Markdown' });
     }
-    await ctx.telegram.setMyCommands(COMMANDS);
-    await ctx.sendMessage("Ok");
-}
-
-async function commandHelpHandler(ctx) {
-    const text = COMMANDS.reduce(
-        (accumulator, item) => `${accumulator}${item.command} - ${item.description}\n`,
-        "Here are commands I understand:\n",
-    );
-    await ctx.sendMessage(text);
-}
-
-async function hearsTokenHandler(ctx) {
-    // presumably token was passed
-    const token = ctx.message.text;
-    if (hasToken(ctx)) {
-        if (token === ctx.session.tokens[0].token) {
-            await ctx.sendMessage("The same token is already stored", {
-                reply_to_message_id: ctx.message.message_id,
-            });
-            return;
+    if (command.startsWith('p')) {
+        const pid = command.slice(1)
+        const project = await TogglTrackAPI.project(pid)
+        if(project) {
+            ctx.session.pid = pid
+            return await ctx.sendMessage(project.name + ' is set');
+        } else {
+            return await ctx.sendMessage('invalid id');
         }
     }
-    // noinspection ES6MissingAwait
-    ctx.telegram.sendChatAction(ctx.chat.id, "typing");
-    const togglUser = await TogglTrackAPI.me(token);
-    if (!togglUser) {
-        await ctx.sendMessage(
-            "Seems like incorrect token...",
-            {reply_to_message_id: ctx.message.message_id},
-        );
-        return;
-    }
-    // noinspection JSUnresolvedReference
-    const tokenObj = {
-        token: token,
-        username: togglUser.fullname,
-    };
-    ctx.session.tokens = [tokenObj];
-    const checkNowReplyMarkup = Markup.inlineKeyboard([
-        Markup.button.callback("Check timer now", "check"),
-    ]);
-    await ctx.sendMessage(`Valid token for "${tokenObj.username}" is saved`, checkNowReplyMarkup);
-}
-
-async function actionCheckHandler(ctx) {
-    await ctx.editMessageReplyMarkup();
-    await commandCheckHandler(ctx);
-}
-
-async function actionStartCheckingHandler(ctx) {
-    const message = ctx.update.callback_query.message;
-    const chatId = message.chat.id;
-    const messageId = message.message_id;
-    const tokenObj = ctx.session.tokens[0];
-    if (tokenObj.checking) {
-        await ctx.editMessageReplyMarkup(stopCheckingReplyMarkup.reply_markup);
-        await ctx.answerCbQuery("Continue checking is active!\n" + "I will inform when the timer stops");
-        return;
-    }
-    const currentTimer = await TogglTrackAPI.currentTimer(tokenObj.token);
-    if (!currentTimer) {
-        await ctx.editMessageText("Timer is already stopped", checkAgainReplyMarkup);
-        return;
-    }
-    tokenObj.checking = true;
-    tokenObj.lastDuration = currentTimer.duration;
-    let msg = buildTimerMessage(tokenObj);
-    await ctx.editMessageText(msg, stopCheckingReplyMarkup);
-    const interval = humanDuration(settings.checkInterval);
-    await ctx.answerCbQuery(`I will check every ${interval} and inform when the timer stops`);
-    checkTimerStatusSetTimeout(ctx.sessionId, tokenObj.token);
-}
-
-async function actionStopCheckingHandler(ctx) {
-    const tokenObj = ctx.session.tokens[0];
-    if (!tokenObj.checking) {
-        await ctx.editMessageText("Continuous checking is not active!", checkAgainReplyMarkup);
-        return;
-    }
-    delete tokenObj.checking;
-    await ctx.editMessageText("Continuous checking was stopped", checkAgainReplyMarkup);
-}
-
-async function actionUnknownHandler(ctx) {
-    await ctx.editMessageReplyMarkup();
-    await ctx.answerCbQuery("Unknown action");
-}
-
-async function onUnknownHandler(ctx) {
-    await ctx.sendMessage("Unknown command, use /help");
-}
-
-async function onEditedMessageHandler(ctx) {
-    await ctx.sendMessage("Editing messages is not supported");
+    return await ctx.sendMessage('unknown command');
 }
 
 async function catchErrorHandler(err, ctx) {
-    let msg = ("Something went wrong...\n" + "Try again later");
-    if (settings.adminUsername) {
-        msg += "\n";
-        msg += `Or let admin know about it: @${settings.adminUsername}`;
-    }
-    try {
-        if (ctx.update.callback_query) {
-            await ctx.answerCbQuery(msg, {show_alert: true});
-        } else {
-            await ctx.sendMessage(msg);
-        }
-    } catch (e) {
-        console.error("Telegram error", e);
-    }
-    if (settings.debug) {
-        throw err;
-    }
-    console.error("bot.catch", err);
+    const message = String(err?.response?.data || err)
+    await ctx.sendMessage("Error: " + message);
 }
-
-const COMMANDS = [
-    {
-        command: "/start",
-        description: "Start bot",
-    },
-    {
-        command: "/check",
-        description: "Check timer for the stored token",
-    },
-    {
-        command: "/stop",
-        description: "Stop current check",
-    },
-    {
-        command: "/edit",
-        description: "How to edit a token?",
-    },
-    {
-        command: "/delete",
-        description: "Delete my token from the storage",
-    },
-];
 
 const bot = new Telegraf(settings.botToken);
 
 bot.use(localSession.middleware());
 bot.use(setup);
 
-bot.command("start", commandStartHandler);
-bot.command("edit", commandEditHandler);
-bot.command("check", checkHasToken, commandCheckHandler);
-bot.command("stop", checkHasToken, commandStopHandler);
-bot.command("delete", checkHasToken, commandDeleteHandler);
-bot.command("help", commandHelpHandler);
-bot.command("settings", commandSettingsHandler);
-
-bot.hears(/^[0-9a-f]{32}$/, hearsTokenHandler);
-
-bot.action("check", checkHasToken, actionCheckHandler);
-bot.action("start_checking", checkHasToken, actionStartCheckingHandler);
-bot.action("stop_checking", checkHasToken, actionStopCheckingHandler);
-bot.action(/.*/, actionUnknownHandler);
-
-bot.on(message(), onUnknownHandler);
-bot.on(editedMessage(), onEditedMessageHandler);
+bot.on(message(), onMessageHandler);
 
 bot.catch(catchErrorHandler);
 
 // noinspection JSIgnoredPromiseFromCall
 bot.launch({
-    allowedUpdates: ["message", "edited_message", "callback_query"],
+    allowedUpdates: ["message"],
 });
 
 // Enable graceful stop
 function gracefullyStop(signal) {
     return function () {
-        timeouts.forEach(clearTimeout);
         bot.stop(signal);
     };
 }
--- src/toggl_track_api.js
+++ src/toggl_track_api.js
@@ -1,28 +1,84 @@
-const { default: axios } = require("axios");
+const dotenv = require("dotenv");
+dotenv.config();
+
+const axios = require("axios");
+
+if (!process.env.TOGGL_TOKEN) {
+  throw new Error("env.TOGGL_TOKEN is not set");
+}
 
 class TogglTrackAPI {
-  static BASE_URL = "https://api.track.toggl.com/api/v9/";
+  static axiosInstance = axios.create({
+    baseURL: "https://api.track.toggl.com/api/v9/",
+    auth: {
+      username: process.env.TOGGL_TOKEN,
+      password: "api_token",
+    },
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
 
-  static async _request(token, path) {
-    try {
-      const response = await axios.get(TogglTrackAPI.BASE_URL + path, {
-        auth: {
-          username: token,
-          password: "api_token",
-        },
-      });
-      return response.data;
-    } catch (error) {
-      console.error("TogglTrackAPI error", error);
-    }
+  static async _get(path) {
+    const response = await this.axiosInstance.get(path);
+    return response.data;
+  }
+  static async _delete(path) {
+    const response = await this.axiosInstance.delete(path);
+    return response.data;
   }
 
-  static me = (token) => {
-    return TogglTrackAPI._request(token, "me");
+  static async _post(path, data = {}) {
+    const response = await this.axiosInstance.post(path, data);
+    return response.data;
+  }
+  static async _patch(path, data = {}) {
+    const response = await this.axiosInstance.patch(path, data);
+    return response.data;
+  }
+  static async _put(path, data = {}) {
+    const response = await this.axiosInstance.put(path, data);
+    return response.data;
   }
 
-  static currentTimer = (token) => {
-    return TogglTrackAPI._request(token, "me/time_entries/current");
+  static me() {
+    return this._get("/me");
+  }
+
+  static currentTimer = () => {
+    return this._get("/me/time_entries/current");
+  }
+
+  static async projects() {
+    const { default_workspace_id } = await this.me();
+    return this._get("/workspaces/" + default_workspace_id + "/projects");
+  }
+
+  static async project(pid) {
+    const { default_workspace_id } = await this.me();
+    return this._get("/workspaces/" + default_workspace_id + "/projects/" + pid);
+  }
+
+  static async projectsString() {
+    const projects = await this.projects();
+    return projects.map(p => p.name + ' `p' + p.id + '`').join('\n');
+  }
+
+  static async startTracking({start, pid}) {
+    const { default_workspace_id } = await this.me();
+    return await this._post("/workspaces/" + default_workspace_id + "/time_entries", {start, pid, created_with: 'mytelegrambot', duration: -1, wid: default_workspace_id});
+  }
+  static async stopTracking(time_entry, time) {
+    const { default_workspace_id } = await this.me();
+    return await this._put("/workspaces/" + default_workspace_id + "/time_entries/" + time_entry.id, {
+      ...time_entry,
+      duration: undefined,
+      stop: time,
+    });
+  }
+  static async discardTracking(time_entry, time) {
+    const { default_workspace_id } = await this.me();
+    return await this._delete("/workspaces/" + default_workspace_id + "/time_entries/" + time_entry.id);
   }
 }
 
